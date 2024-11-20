b200b8adbb157a435beb8a2403916c6efe16ad8e
JoyvineNaamah <joyvinenamaemba0@gmail.com>
2024-11-20 16:50:41 +0300

    Assignment Week Two

---
+++ assignment.py
@@ -0,0 +1,26 @@
+# Creating an empty list
+my_list = []
+
+# Appending the following elements to my_list: 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Inserting the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# Extending my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Removing the last element from my_list
+my_list.pop()
+
+# Sorting my_list in ascending order
+my_list.sort()
+
+# Finding and printing the index of the value 30 in my_list
+print(my_list.index(30))
+
+# Printing the list
+print(my_list)
